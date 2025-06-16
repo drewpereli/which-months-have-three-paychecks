6b75a9da7808e658be25a9bf7b3d686a221a61d7
Drew Pereli <drewpereli@gmail.com>
2025-06-16 00:06:49 -0700

    Run formatter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import "./style.css";
 
-import Alpine from 'alpinejs'
-import mask from '@alpinejs/mask'
+import Alpine from "alpinejs";
+import mask from "@alpinejs/mask";
 
 Alpine.plugin(mask);
 
-(window as unknown as {Alpine: typeof Alpine}).Alpine = Alpine
- 
+(window as unknown as { Alpine: typeof Alpine }).Alpine = Alpine;
+
 Alpine.start();
