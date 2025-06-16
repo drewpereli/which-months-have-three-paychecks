1744b22b2b80b4ff7ad2137acbf88a933b03c863
Drew Pereli <drewpereli@gmail.com>
2025-06-15 23:59:30 -0700

    Add basic input

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import "./style.css";
+
+import Alpine from 'alpinejs'
+import mask from '@alpinejs/mask'
+
+Alpine.plugin(mask);
+
+(window as unknown as {Alpine: typeof Alpine}).Alpine = Alpine
+ 
+Alpine.start();
